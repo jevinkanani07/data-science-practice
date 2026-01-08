4b6f05d19995d8302b182435f537d9f485d26487
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-08 17:32:22 +0530

    Print Statements and Variables

---
+++ python/Day01_Print_Variables.py
@@ -0,0 +1,14 @@
+# Day 01 
+# Topic :- Print Statements and Variables 
+
+print("Hello, Here a first Code")
+
+name = "Jevin"
+education = "Data Science And BCA"
+
+print("My Name Is",name)
+print("My Education Is About ",education)
+
+age = 19
+
+print(f"I Am {age} Years old ")
