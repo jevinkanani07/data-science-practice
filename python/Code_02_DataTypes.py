7915bca7b720663ca629a589cb5a63e33714f1e5
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-11 11:18:46 +0530

    Practiced Code 2 for Data Types and type() function

---
+++ python/Code_02_DataTypes.py
@@ -0,0 +1,18 @@
+# Code 2 
+# Topic : Data Types And Type() Function
+
+age = 19
+print("Age :",age)
+print("Type of age:",type(age))
+
+height = 5.8
+print("Height :",height)
+print("Type of height :",type(height))
+
+name = "Jevin"
+print("Name:",name)
+print("Type of Name :",type(name))
+
+is_student = True
+print("Is student :",is_student)
+print("Type of is_student:",type(is_student))
