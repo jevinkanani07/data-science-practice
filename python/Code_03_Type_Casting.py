35c4d0271fe12c9a90561da7edecb83e879239d7
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-12 20:47:19 +0530

    Add type casting examples in Code_03_Type_Casting.py
    Demonstrates type casting between string, integer, and float in Python.

---
+++ python/Code_03_Type_Casting.py
@@ -0,0 +1,34 @@
+# Code 03
+# Topic : Type Casting (int,float,str)
+
+# String to Integer
+num_str = "10"
+num_int = int(num_str)
+
+print("num_str:", num_str, "Type:",type(num_str))
+print("num_int:", num_int, "Type:",type(num_int))
+
+# Integer to Float
+marks_int = 85
+marks_float = float(marks_int)
+
+print("marks_int:", marks_int, "Type:", type(marks_int))
+print("marks_float:", marks_float, "Type:", type(marks_float))
+
+# Integer to String
+age = 19
+age_str = str(age)
+
+print("age:", age, "Type", type(age))
+print("age_str:", age_str, "Type", type(age_str))
+
+# Float to Integer
+height = 5.8
+height_int = int(height)
+
+print("height:" ,height, "Type", type(height))
+print("height_int:",height_int, "Type", type(height_int))
+
+
+
+
