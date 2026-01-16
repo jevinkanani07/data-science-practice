b49bc8cb151b595030d564331f5302720aac3753
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-16 10:37:12 +0530

    work with arithmetic, comparison and logical operators

---
+++ python/Code_04_Operators.py
@@ -0,0 +1,30 @@
+# Code 04
+# Topic: Python Operators
+
+a = 10
+b = 15
+
+# Arithmetic Operators
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Divison:",a/b)
+print("Modulus:",a%b)
+print("Floor Division:",a//b)
+print("Power:",a**b)
+
+# Comparison Operators
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+# Logical Operators
+x = True
+y = False
+
+print("x and y:",x and y)
+print("x or y:",x or y)
+print("not x:", not x)
