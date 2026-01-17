7a62e86f0547569575b8f3a174a1e87d91655eb5
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-17 20:50:05 +0530

    used if, elif and else to implement basic decision-making logic.
    Added examples of if, if-else, and if-elif-else conditional statements.

---
+++ python/Code_05_Conditional_Statements.py
@@ -0,0 +1,28 @@
+# Code 05
+# Topic: Conditional Statements (if,elif,else)
+
+age = 19
+
+# if conditional
+if age >= 18:
+  print("You are eligible to vote")
+
+# if-else conditional
+marks = 75
+
+if marks >= 40:
+  print("You are passsed ")
+else:
+  print("You are failed ")
+
+# if-elif-else conditional
+score = 82
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 75:
+  print("Grade: B")
+elif score >= 60:
+  print("Grade: C")
+else:
+  print("Grade: D")
