ec789eb58239019e3690d52d1ed12333952f4ba6
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-23 16:50:41 +0530

    work with for loop, while loop, break and continue statements in python

---
+++ python/Code_06_Loops.py
@@ -0,0 +1,29 @@
+# Code 06
+# Topic : Loops in Python
+
+# for loop
+print("For loop example :")
+for i in range(1,6):
+    print("Number:",i)
+
+# while loop
+print("While loop example:")
+count = 1
+while count <= 5:
+    print("Count",count)
+    count += 1
+
+# break example
+print("Break example:")
+for i in range(1,10):
+    if i ==5:
+        break
+    print(i)
+
+# countinue example
+print("Countinue example")
+for i in range(1 , 10):
+    if i == 5:
+        continue
+    print(i)
+
