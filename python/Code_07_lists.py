b92eebef4655f4d755d8eab88c32a6ab90248ea0
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-24 16:09:38 +0530

    Created and modified lists, accessed elements and iterated using loops.
    Added examples of list creation, modification, and iteration in Python.

--- python/Code_07_lists.py
+++ python/Code_07_lists.py
@@ -1,1 +1,30 @@
+# Code 07
+# Topic : Lists in Python
 
+# create a list
+numbers = [10, 20, 30, 40, 50]
+print("Numbers list:",numbers)
+
+# Access elements
+print("First element:",numbers[0])
+print("Last elements:",numbers[-1)
+
+# Modify elements
+numbers[2] = 35
+print("After modification:",numbers)
+
+# Add elements
+numbers.remove(20)
+print("After append:",numbers)
+
+# Remove elements
+numbers.remove(20)
+print("After remove:",numbers)
+
+# list length
+print("Length of list:",len(numbers))
+
+# loop through list
+print("Looping thtough List:")
+for num in numbers:
+    print(num)
