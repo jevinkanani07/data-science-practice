437f52c029a380f521bb5f0c7a1354908147ae8b
Jevin Kanani <jevinkanani07@gmail.com>
2026-01-26 11:03:56 +0530

    Created Tuple, accessed elements and iterated through tuple values

---
+++ python/Code_08_Tuples.py
@@ -0,0 +1,28 @@
+# Code 08
+# Topic : Tuples in Python
+
+# Create a Tuple
+student = ("Jevin",19,"Data Science")
+print("Student Tuple:",student)
+
+# Access elements
+print("Name:",student[0])
+print("Age:",student[1])
+print("Course:",student[2])
+
+# Tuple length
+print("Length of tuple:",len(student))
+
+# Tuple with mixed data tyeps
+marks = (85,90,78,92)
+print("Marks tuple:",marks)
+
+# Access last element
+print("Last mark:",marks[-1])
+
+# Loop through tuple
+print("Looping through marks:")
+for m in marks:
+    print(m)
+
+# Tuple is immutable 
